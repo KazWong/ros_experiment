73b5ae5607e8eb39dde40482e2d248224c3e2dc9
Kaz Wong <kaz_wong@sae.com.hk>
2018-02-12 17:20:22 +0800

    exp

--- src/tf_exp/src/tf_exp.cpp
+++ src/tf_exp/src/tf_exp.cpp
@@ -16,7 +16,7 @@ typedef geometry_msgs::Twist s_Twist;
 
 
 const double wait_tf = 0.02; //12Hz max 0.042 each in WaitForTranform
-const double listener_buf = 0.1;
+const double listener_buf = 1.5;
 
 string parking_no;
 string ref_frame;
